e6a3403cf43d58d73936bd390bf92213e54afe7c
do-d1 <nonick95@gmail.com>
2026-05-30 07:20:44 +0300

    fix: package name com.example.dbneighbors (no underscore)

--- android/app/src/main/kotlin/com/example/db_neighbors/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/db_neighbors/MainActivity.kt
@@ -1,3 +1,1 @@
-package com.example.db_neighbors
-import io.flutter.embedding.android.FlutterActivity
-class MainActivity: FlutterActivity()
+// removed
---
+++ android/app/src/main/kotlin/com/example/dbneighbors/MainActivity.kt
@@ -0,0 +1,3 @@
+package com.example.dbneighbors
+import io.flutter.embedding.android.FlutterActivity
+class MainActivity: FlutterActivity()
